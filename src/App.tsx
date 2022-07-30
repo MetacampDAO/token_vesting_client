dfdf8028a6673aa9b1dcee8e7fa7e21cb62a806c
Justin Lee <justinleeyile@gmail.com>
2022-07-30 20:23:41 +0800

    instantiate client

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAnchorWallet } from '@solana/wallet-adapter-react';
+import { initVestingClient } from './client/init';
+import { Wallet } from '@project-serum/anchor';
+import { VestingClient } from './client/tokenVesting.client';
 
 function App() {
+    const wallet = useAnchorWallet();
+    const [client, setClient] = useState<VestingClient | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            if (wallet) {
+                try {
+                    const vestingClient = await initVestingClient(wallet as Wallet);
+                    setClient(vestingClient);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        })();
+    }, [wallet]);
+
     return (
         <div
             style={{
